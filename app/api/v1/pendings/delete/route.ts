c9b00aba1bafad1956faa1cd89fb8173832921c3
Ivan Christian Pabayo <zzzpabayo2@gmail.com>
2026-07-08 15:11:16 +0800

    FEAT: added delete button to remove orders of specific inquiry. only remove orders, but not their inquiry message. orders can be recreated again manually

---
+++ app/api/v1/pendings/delete/route.ts
@@ -0,0 +1,26 @@
+import { deletingPending } from "@/dal/menu/pendings/delete/delete-pending";
+import { revalidatePath } from "next/cache";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function DELETE(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+
+  console.log(`from DELETE route`);
+
+  const ordersId = searchParams.get("orders_id");
+  console.log(ordersId);
+
+  if (!ordersId)
+    return NextResponse.json({
+      type: "error",
+      message: "Nullish id??",
+    });
+
+  const deletedPending = await deletingPending(ordersId);
+
+  revalidatePath("/v1/pendings");
+
+  return NextResponse.json({
+    message: deletedPending,
+  });
+}
