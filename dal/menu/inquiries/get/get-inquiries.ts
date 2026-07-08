bf83e7cb91e9e0fe851ab2d160e7e25131c8dec8
Ivan Christian Pabayo <zzzpabayo2@gmail.com>
2026-07-08 19:17:46 +0800

    FEAT: filters out done inquiries to not be displayed in inquiry page

--- dal/menu/inquiries/get/get-inquiries.ts
+++ dal/menu/inquiries/get/get-inquiries.ts
@@ -1,7 +1,9 @@
 import prisma from "@/lib/db";
 
 export async function getInquiries() {
-  const inquiries = await prisma.clientEmailRequest.findMany();
+  const inquiries = await prisma.clientEmailRequest.findMany({
+    where: { is_done: false },
+  });
 
   return inquiries;
 }
