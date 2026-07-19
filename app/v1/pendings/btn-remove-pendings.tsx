71da92fc902e26fe341db12da49f4b9fdfe586d2
Ivan Christian Pabayo <zzzpabayo2@gmail.com>
2026-07-19 23:28:17 +0800

    FEAT: data synching with buttons — mark done and delete/remove

--- app/v1/pendings/btn-remove-pendings.tsx
+++ app/v1/pendings/btn-remove-pendings.tsx
@@ -1,13 +1,14 @@
-"use client";
-
 import { DeleteIcon } from "../products/ui/shared";
 import { deleteIconButtonClassName } from "./pending-cards";
+import { useRouter } from "next/navigation";
 
 export default function EfBtnRemovePendings({
   pendingId,
 }: {
   pendingId: string;
 }) {
+  const router = useRouter();
+
   const handleDeleteClick = async (ordersId: string) => {
     const params = new URLSearchParams({
       orders_id: ordersId,
@@ -25,6 +26,8 @@ export default function EfBtnRemovePendings({
     const data = await response.json();
 
     console.log(data);
+
+    router.refresh();
   };
 
   return (
